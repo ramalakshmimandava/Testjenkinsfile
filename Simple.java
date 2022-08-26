74cc825f6d18156dc5a443256208eef6cc0a006f
ramalakshmimandava <99663286+ramalakshmimandava@users.noreply.github.com>
2022-08-26 10:45:28 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,6 +1,7 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java");
-     System.out.println("Welcome Java"); 
+     System.out.println("Welcome Java");
+        System.out.println("This is a simple java file"); 
     }  
 }  	
